fcd2e616d8466e2c7ffff4d4b5c2746ecf8d1973
John Chinonso Edeh <netojaycee@gmail.com>
2025-12-15 15:15:33 +0100

    gt

--- app/api/v1/cookies/route.ts
+++ app/api/v1/cookies/route.ts
@@ -1,6 +1,9 @@
 // basic post route to set a cookie
 import { getAppSession } from "@/lib/utils/cookies";
 import { NextResponse } from "next/server";
+
+export const dynamic = 'force-dynamic';
+
 export async function GET(request: Request) {
   console.log("GET /api/v1/cookies called", request);
   const { user, group, entity } = await getAppSession();
