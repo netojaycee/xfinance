2a84e12c4917d032d33cfcd9519f9366096fad4e
John Chinonso Edeh <netojaycee@gmail.com>
2025-12-15 15:03:07 +0100

    H

--- app/api/v1/cookies/route.ts
+++ app/api/v1/cookies/route.ts
@@ -2,6 +2,7 @@
 import { getAppSession } from "@/lib/utils/cookies";
 import { NextResponse } from "next/server";
 export async function GET(request: Request) {
+  console.log("GET /api/v1/cookies called", request);
   const { user, group, entity } = await getAppSession();
   const response = NextResponse.json({ user, group, entity });
   return response;
